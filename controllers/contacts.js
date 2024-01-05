ad87842e117a27d285fffa9e13dd6823d2fce637
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-01-05 17:06:03 +0300

    6

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -20,6 +20,18 @@ const addContact = async (req, res) => {
   res.status(201).json(contact);
 }
 
+const removeContact = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const contact  = await Contact.findByIdAndRemove(id);
+  if (!contact ) {
+      throw HttpError(404, "Not found");
+  }
+  res.json({
+      message: "Delete success"
+  })
+}
+
 // const updateById = async (req, res) => {
 //   const { id } = req.params;
 //   const result = await Book.findByIdAndUpdate(id, req.body, {new: true});
@@ -38,22 +50,13 @@ const addContact = async (req, res) => {
 //   res.json(result);
 // }
 
-// const deleteById = async (req, res) => {
-//   const { id } = req.params;
-//   const result = await Book.findByIdAndRemove(id);
-//   if (!result) {
-//       throw HttpError(404, "Not found");
-//   }
-//   res.json({
-//       message: "Delete success"
-//   })
-// }
+
 
 
 module.exports = {
   listContacts: ctrlWrapper(listContacts),
   getContactById: ctrlWrapper(getContactById),
-  // removeContact,
+ removeContact: ctrlWrapper(removeContact),
  addContact: ctrlWrapper(addContact),
   // updateContact,
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,12 +8,12 @@ const {schemas} = require("../../models/contact");
 router.get("/", ctrl.listContacts);
 router.get("/:id", isValidId, ctrl.getContactById);
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
-
-// router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.delete("/:id", isValidId, ctrl.removeContact);
+//router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
 // router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
-// router.delete("/:id", isValidId, ctrl.deleteById);
+ 
 
 
 module.exports = router
