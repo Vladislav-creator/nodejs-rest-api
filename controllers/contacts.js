5cf29751fc638feeacaa31ff93f32273f238475e
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2024-02-05 21:23:28 +0300

    234

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
   const {_id: owner} = req.user;
-  const {page = 1, limit = 20, favorite = false} = req.query;
+  const {page = 1, limit = 20, favorite = true} = req.query;
 
   const skip = (page - 1) * limit;
   const contacts = await Contact.find({owner, favorite},"-createdAt -updatedAt",{skip, limit: Number(limit)});
